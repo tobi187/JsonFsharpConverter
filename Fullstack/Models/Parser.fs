10f9ca65e5efc2f48ff2b2afed93276a70869091
Tobias Fischer <fischerstobi1998@gmail.com>
2024-03-18 17:22:31 +0100

    update

--- Fullstack/Models/Parser.fs
+++ Fullstack/Models/Parser.fs
@@ -2,7 +2,6 @@
 module Parser
 open Chiron
 
-
 let keyWords = Set ["asr";"land";"lor";"lsl";"lsr";"lxor";"mod";"sig";"const";"yield!";"yield";"with";"while";"when";"void";"val";"use!";"use";"upcast";"type";"try";"true";"false";"to ";"then";"struct";"static";"select";"return";"return!";"rec";"public";"private";"override";"or";"open";"new";"of";"null";"not";"namespace";"mutable";"module";"member";"match!";"match";"let!";"let";"lazy";"internal";"interface";"inline";"inherit";"in";"if";"global";"function";"fun";"for";"fixed";"finally";"extern";"exception";"end";"else";"elif";"downto";"downcast";"done";"do";"delegate";"default";"class";"begin";"base";"assert";"as";"and";"abstract"]
 
 type Element = {
--- jsontest.fsx
+++ jsontest.fsx
@@ -1,7 +1,3 @@
-#r "nuget: Chiron, 6.3.1"
-
-open Chiron
-
 open System.Text
 open System.Text.Json
 open System.Text.Json.Nodes
@@ -10,10 +6,13 @@ open System.Collections.Generic
 let jsonFile =
     """{"menu":{"id":"file","value":"File","popup":{"menuitem":[{"value":null,"onclick":"CreateDoc()"},{"value":"Open","onclick":"OpenDoc()"},{"value":"Save","onclick":"SaveDoc()"},{"value":"Save","onclick":"SaveDoc()"},{"value":"Schnick","onclick":"Schnack()"}]}}}"""
 
-let parsed = Json.parse jsonFile
 
-
-type Elem = { name: string; dType: string }
+type Elem = 
+    { name: string; dType: string }
+    with 
+    member this.isOption = this.dType.EndsWith "option"
+    member this.isUnknown = this.dType = "obj option"
+        
 
 [<CustomEquality;NoComparison>]
 type Group =
@@ -49,8 +48,33 @@ let seen = new HashSet<Group> ()
 
 let rec unpack parsedJson result = ()
 
+let updateType (e1 : Elem, e2: Elem) =
+    // case
+        // same type -> any,
+        // one x option, other x or option -> one with x option
+        // one x option, other type y -> ignore for now
+        // one x, other option -> x + option
+    match e1, e2 with
+    | f, s when f.dType = s.dType -> e1
+    | f, _ when f.isOption && not f.isUnknown -> f
+    | _, s when s.isOption && not s.isUnknown -> s
+    | f, s when f.isUnknown -> { f with dType = $"{s.dType} option" } 
+    | f, s when s.isUnknown -> { s with dType = $"{f.dType} option" } 
+    | x, y -> failwith (sprintf "%A - %A" x y)
+
 let update old neww =
-    seen.Remove old || seen.Add neww
+    let tcorr = {
+        neww with kids = neww.kids
+                        |> List.zip old.kids
+                        |> List.map (updateType) 
+    }
+
+    seen.Remove old || seen.Add tcorr
+
+let addOrUpdate el =
+    match seen.TryGetValue el with
+    | true, x -> update x el
+    | false, _ -> seen.Add el
 
 let js = JsonSerializer.Deserialize jsonFile :> JsonValue
 
@@ -71,3 +95,44 @@ let detectJsonType (root: JsonValue) =
 
     { name = root.GetPropertyName()
       dType = pType }
+
+let detectJsonTypeP (root: JsonProperty) =
+    let pType =
+        match root.Value.ValueKind with
+        | JsonValueKind.Array -> root.Name + " list"  // "array" // array list seq ?
+        | JsonValueKind.Object -> root.Name // "object" // insert object name
+        | JsonValueKind.False
+        | JsonValueKind.True -> "bool"
+        | JsonValueKind.Null -> "obj option"  // other type option
+        | JsonValueKind.Number -> "int" // int int64 decimal float 
+        | JsonValueKind.String -> "string"
+        | JsonValueKind.Undefined -> failwith "undefined"
+        | uint -> failwith (sprintf "JsonValue Match Whati is going on %A" (root.Value))
+
+    { name = root.Name
+      dType = pType }
+
+let unwrapArray (value: JsonValue) (name: string) =
+    0
+
+let unwrapObject (value: JsonValue) (name:string) =
+    let a = value.GetValue() :> JsonElement
+    let fields = 
+        a.EnumerateObject()
+        |> Seq.map (detectJsonTypeP)
+
+    let g = {
+        name = name;
+        dType = name;
+        kids = fields;
+        internalHash = ""
+    }
+
+    let gg = { g with internalHash = getGroupHash g }
+
+    addOrUpdate gg |> printfn "Update successful: %b"
+
+    let objs =
+        a.EnumerateObject()
+        |> Seq.filter (fun s -> s.Value.ValueKind = JsonValueKind.Object)
+
